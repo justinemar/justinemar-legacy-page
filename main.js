abaa162ce3a0fd65475566d5444d13cc2dde5a1e
Justine Cantado <justinecantado@gmail.com>
2017-09-15 09:51:10 +0000

    update

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 
 
-function cssEffect(pos,elem,aclass){
-    $(window).scroll(function(){
-        
-        if($(window).scrollTop() > pos){
-        $(elem).addClass(aclass);
-    }else{
-        $($(elem)).removeClass(aclass);
-    }
-    })
+function addclass_scroll(pos,elem,_class){
+    $(window).on('scroll',function(){
+    if($(window).scrollTop() > pos)
+        $(elem).addClass(_class);
+    else
+        $($(elem)).removeClass(_class);
+    });
 }
 
 
@@ -18,7 +16,29 @@ $(document).ready(function(){
     $("button").click(function(){
         $(".msg-box").fadeOut();
     });
+    addclass_scroll(300,".parallax-1","blur");
+    addclass_scroll(235,".fake-header","nav-fixed")
+    // Just a little fun
+   var updateFollower = setInterval(function(){
+         $(".count").first().text("1.2M");
+    }, 500)
     
-    cssEffect(300,".parallax-1","blur");
+    
+     $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+      });
+    } // End if
+  });
 });
