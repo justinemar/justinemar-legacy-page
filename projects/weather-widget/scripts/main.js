e72b6c9398b791ef110b756f9fbb0bddf82cf14e
Justine Cantado <justinecantado@gmail.com>
2017-10-02 14:23:00 +0000

    new project weather widget

---
+++ projects/weather-widget/scripts/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+var $city = $("#city");
+var $country = $("#country");
+var $temperature = $("#temp");
+var $report = $("#desc");
+var $icon = $("#icon");
+var $unit = $("#temunit");
+    if (navigator.geolocation) {
+        var lat;
+        var long;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            weather(lat, long,$city,$country,$temperature,$report,$icon,$unit); //Call function if navigator is available//
+        });
+    }else{
+        alert("Your browser doesn't support navigation");
+    }
+    
+
+
+});
+
+
+function weather(lat, long,$city,$country,$temperature,$report,$icon,$unit) {
+    let api = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&" + "lon=" + long;
+    $.getJSON(api, function(data) {
+        $($city).html(data.name + ", ");
+        $($country).html(data.sys.country);
+        $($temperature).html(data.main.temp + "°")
+        $($report).html(data.weather[0].description);
+        $($icon).attr("src", data.weather[0].icon);
+        convert(data.main.temp,$unit,$temperature)
+    });
+    
+
+}
+
+
+function convert(temp, $unit,$temperature){
+    $($unit).on('click', function(){
+        if($(this).html() === "C"){
+        temp = (temp * 1.8) + 32;
+        $($temperature).html(temp + "°");
+        $($unit).html("F");
+        }else{
+            temp = Math.round((temp - 32) * 5 / 9);
+        $($temperature).html(temp + "°");
+        $($unit).html("C");
+        }
+})
+}
