046c6f8288a28c76f5c2779b948ab83d1dfcf7c1
Justine Cantado <justinecantado@gmail.com>
2017-10-11 04:21:21 +0000

    new project init

--- projects/shapes/scripts/main.js
+++ projects/shapes/scripts/main.js
@@ -2,15 +2,58 @@
 
 
 $(document).ready(function(){
-    var introCircle = $(".circle");
+    //Some game ui animation//
+    var introCircle = $(".shapes-animated #circle");
     var introParallel = $(".parallelogram");
-    var introSquare = $(".square");
-    var playBtn = $("#play-game");
+    var introSquare = $(".shapes-animated #square");
     introCircle.css("top","0px");
     introSquare.css("right","0px");
-    
-    
+    //----------------------------//
+    var gameMain = $(".game-main-ui");
+    var playBtn = $("#play-game");
     $(playBtn).on('click', function(){
         $('.game-start-ui').fadeOut();
-    })  
+        gameMain.css("visibility","visible").hide().fadeIn(3000);
+    });
+    var toolContent = $('.tool-content');
+    var workspace = $(".workspace");
+    
+    draggable(toolContent,workspace);
+
+    
+    $('.workspace').droppable({
+        accept: toolContent,
+        drop: function(event,ui){
+        var shape = $(ui.helper).clone();
+        shape.draggable({
+            containment: workspace,
+            stack: toolContent,
+            snap: true,
+            snapMode: 'outer',
+            snapTolerance: 13,
+        });
+        $(this).append(shape);
+    }
+    })
+    
+    
+	$('.tool-box').on('mousedown', function () {
+		$('.instruction').fadeOut('slow');
+	})
+
+
+
 })
+
+function draggable(toolContent,workspace){
+    	toolContent.draggable({
+		containment: workspace,
+		stack: '.tool-content',
+		snap: true,
+		snapMode: 'outer',
+		snapTolerance: 13,
+		helper: 'clone',
+	});
+}
+
+
